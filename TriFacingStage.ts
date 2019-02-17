efe29ef76a821d65ce605a60c68572fcea2ea034
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 15:18:28 +0530

    adding TriFacingStage class

--- TriFacingStage.ts
+++ TriFacingStage.ts
@@ -56,3 +56,33 @@ const drawTFNode : Function = (context : CanvasRenderingContext2D, i : number, s
     }
     context.restore()
 }
+
+class TriFacingStage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : TriFacingStage = new TriFacingStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
