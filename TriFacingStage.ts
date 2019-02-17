34cd652c6cdd6c2a7329f168de3d48e61759103e
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 14:14:15 +0530

    adding constants

---
+++ TriFacingStage.ts
@@ -0,0 +1,8 @@
+const nodes : number = 5
+const triangles : number = 2
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const sizeFactor : number = 2.9
+const strokeFactor : number = 90
+const foreColor : string = "#4CAF50"
+const backColor : string = "#212121"
