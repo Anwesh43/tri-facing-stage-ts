9b60ea682334abc824d127e26703da5c8707752e
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 15:59:56 +0530

    using renderer in stage

--- TriFacingStage.ts
+++ TriFacingStage.ts
@@ -60,6 +60,7 @@ const drawTFNode : Function = (context : CanvasRenderingContext2D, i : number, s
 class TriFacingStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -71,11 +72,14 @@ class TriFacingStage {
     render() {
         this.context.fillStyle = backColor
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
