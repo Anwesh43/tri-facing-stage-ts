8850b2dbb1e06c837a1d30e8d05613c5c4fdd6dd
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 14:18:20 +0530

    adding scale methods

--- TriFacingStage.ts
+++ TriFacingStage.ts
@@ -6,3 +6,22 @@ const sizeFactor : number = 2.9
 const strokeFactor : number = 90
 const foreColor : string = "#4CAF50"
 const backColor : string = "#212121"
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1 / n, maxScale(scale, i, n)) * n
+}
+
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
