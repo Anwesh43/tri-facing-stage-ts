233066958844479f5288290746772b44f3992e30
Anwesh43 <anweshthecool0@gmail.com>
2019-02-17 15:38:28 +0530

    adding TriFacing class

--- TriFacingStage.ts
+++ TriFacingStage.ts
@@ -172,3 +172,27 @@ class TFNode {
         return this
     }
 }
+
+class TriFacing {
+
+    root : TFNode = new TFNode(0)
+    curr : TFNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
